82f9f40bd20ea8416e0086b8ee87b65dfcf79554
Mathieu Ducharme <ducharme.mathieu@gmail.com>
2022-03-11 12:13:04 -0500

    Attributes for fighter.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -6,7 +6,58 @@ namespace Combat
     {
         [SerializeField] private Health health;
 
+        [SerializeField] Attributes coreAttributes;
+        [SerializeField] Attributes transientAttributes;
+        private readonly Attributes mergedAttributes = new();
+
         public Health Health { get => health; private set { } }
+        public Attributes Attributes { get => mergedAttributes; private set { } }
+
+        private void Awake()
+        {
+            mergedAttributes.Add(coreAttributes);
+            mergedAttributes.Add(transientAttributes);
+        }
+
+        public Attack GetAttack(CombatType type = CombatType.Base)
+        {
+            Attack attack = new(type);
+            attack.AddModifiers(coreAttributes.attacks);
+            attack.AddModifiers(transientAttributes.attacks);
+            return attack;
+        }
+
+        public Defense GetDefense(CombatType type = CombatType.Base)
+        {
+            Defense defense = new(type);
+            defense.AddModifiers(coreAttributes.defenses);
+            defense.AddModifiers(transientAttributes.defenses);
+            return defense;
+        }
+
+        public void AddCoreAttributes(Attributes attr)
+        {
+            coreAttributes.Add(attr);
+            mergedAttributes.Add(attr);
+        }
+
+        public void RemoveCoreAttributes(Attributes attr)
+        {
+            coreAttributes.Remove(attr);
+            mergedAttributes.Remove(attr);
+        }
+
+        public void AddTransientAttributes(Attributes attr)
+        {
+            transientAttributes.Add(attr);
+            mergedAttributes.Add(attr);
+        }
+
+        public void RemoveTransientAttributes(Attributes attr)
+        {
+            transientAttributes.Remove(attr);
+            mergedAttributes.Remove(attr);
+        }
 
         [System.Serializable]
         public struct SaveData
